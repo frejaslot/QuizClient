a780815dc7f8402e58c9a26d41d5bd43df2a1200
Freja Hallund Slot <frejahallundslot@frejas-mbp-2.hhk.dk>
2017-11-07 17:59:09 +0100

    New

--- js/login.js
+++ js/login.js
@@ -1,16 +1,8 @@
 $(document).ready(() => {
 
-    SDK.User.loadNav();
-
-    $("#signup-button").click(() => {
-
-        window.location.href = "signUp.html";
-
-    });
-
     $("#login-button").click(() => {
-
-        window.location.href = "myPage.html";
+console.log("hej")
+       // window.location.href = "myPage.html";
 
     });
 
